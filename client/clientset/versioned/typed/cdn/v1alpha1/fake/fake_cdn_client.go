69b173e9de8a506898b40d1d6f42839aa51f59ba
lgtm <1gtm@users.noreply.github.com>
2021-07-02 14:48:28 -0700

    Generate code for provider@v1.23.1-0.20210629201646-266cb440f82b gen@ecdba3f (#2)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- client/clientset/versioned/typed/cdn/v1alpha1/fake/fake_cdn_client.go
+++ client/clientset/versioned/typed/cdn/v1alpha1/fake/fake_cdn_client.go
@@ -19,9 +19,10 @@ limitations under the License.
 package fake
 
 import (
+	v1alpha1 "kubeform.dev/provider-digitalocean-api/client/clientset/versioned/typed/cdn/v1alpha1"
+
 	rest "k8s.io/client-go/rest"
 	testing "k8s.io/client-go/testing"
-	v1alpha1 "kubeform.dev/provider-digitalocean-api/client/clientset/versioned/typed/cdn/v1alpha1"
 )
 
 type FakeCdnV1alpha1 struct {
--- client/clientset/versioned/typed/droplet/v1alpha1/droplet.go
+++ client/clientset/versioned/typed/droplet/v1alpha1/droplet.go
@@ -22,12 +22,13 @@ import (
 	"context"
 	"time"
 
+	v1alpha1 "kubeform.dev/provider-digitalocean-api/apis/droplet/v1alpha1"
+	scheme "kubeform.dev/provider-digitalocean-api/client/clientset/versioned/scheme"
+
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	rest "k8s.io/client-go/rest"
-	v1alpha1 "kubeform.dev/provider-digitalocean-api/apis/droplet/v1alpha1"
-	scheme "kubeform.dev/provider-digitalocean-api/client/clientset/versioned/scheme"
 )
 
 // DropletsGetter has a method to return a DropletInterface.
